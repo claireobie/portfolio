f5bd19153f795f8f90901ca5b132b7cbba8775e2
Claire Obrien <claireobrien@Claires-MacBook-Pro.local>
2018-08-20 20:21:42 -0600

    ran build

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const Sequelize = require('sequelize')
-const epilogue = require('epilogue')
-const bcrypt = require('bcrypt')
-const path = require('path')
-const PORT = process.env.PORT || 8081
+var express = require("express");
 
-let app = express()
-app.use(express.static(path.join(__dirname, '../dist')))
-app.use(cors())
-app.use(bodyParser.json())
+var path = require("path");
 
-app.listen(PORT, () => { console.log(`listening to port ${PORT}`)})
+var app = express();
+
+var port = process.env.PORT || 3000
+
+app.use("/", express.static(path.join(__dirname,'/public')));
+
+app.listen(port, () => {console.log(`app running on ${port}`)});
